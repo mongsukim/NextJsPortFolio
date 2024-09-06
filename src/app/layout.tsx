bb38c9a8f6ad6dd453f52b26980de65ef9179e4c
developermong2 <developermong2@gmail.com>
2024-09-06 18:57:43 +0900

    Etc : 페이지 실습

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <header>
+          <nav>
+              <ul className="flex space-x-2">
+                  <li>
+                      <Link href="/">Home</Link>
+                  </li>
+                  <li>
+                      <Link href="/about">About</Link>
+                  </li>
+                  <li>
+                      <Link href="/profile">Profile</Link>
+                  </li>
+                  <li>
+                      <Link href="/products">Products</Link>
+                  </li>
+              </ul>
+          </nav>
+
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {children}
+      </main>
+      <footer>footer</footer>
+      </body>
     </html>
   );
 }
